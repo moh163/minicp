53a93e44e1a590a077e62583edae02bbe9d54c6d
moh163 <97916160+moh163@users.noreply.github.com>
2025-11-04 18:00:04 -0500

    Constructeur

--- src/main/java/minicp/engine/constraints/tp2.java
+++ src/main/java/minicp/engine/constraints/tp2.java
@@ -3,21 +3,23 @@ package minicp.engine.constraints;
 
 import minicp.engine.core.AbstractConstraint;
 import minicp.engine.core.IntVar;
+import minicp.util.exception.InconsistencyException;
 import minicp.util.exception.NotImplementedException;
 public class tp2 extends AbstractConstraint {
-    private final IntVar y;
+    private final IntVar[] y;
     private final int x, z;
 
      /**
      * Creates a constraint such
-     * that {@code x != y + v}
-     * @param x the left member
-     * @param y the right memer
-     * @param v the offset value on y
+     * that z ≥ 0
+     * si x = 0 alors moyenne(y[0], ..., y[n − 1]) ≤ 2 ∗ z sinon moyenne(y[0], ..., y[n − 1]) ≤ z
+     * @param x verify if equals to 0
+     * @param y the variable
+     * @param v the right member
      * @see 
      */
-    public tp2(int x, IntVar y, int v) { 
-        super(y.getSolver());
+    public tp2(int x, IntVar[] y, int v) { 
+        super(y[0].getSolver());
         this.y = y;
         this.x = x;
         this.z = v;
@@ -26,6 +28,9 @@ public class tp2 extends AbstractConstraint {
 
     @Override
     public void post() {
+         if (z < 0) {
+            throw new InconsistencyException();
+        }
     }
 
     @Override
