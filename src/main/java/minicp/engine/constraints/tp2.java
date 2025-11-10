8ba3991117113ec33a624513e90458e328f27479
medxix <mohammedksikes1@gmail.com>
2025-11-10 13:28:11 -0500

    qst3

--- src/main/java/minicp/engine/constraints/tp2.java
+++ src/main/java/minicp/engine/constraints/tp2.java
@@ -1,29 +1,17 @@
 package minicp.engine.constraints;
-
-
 import minicp.engine.core.AbstractConstraint;
 import minicp.engine.core.IntVar;
-import minicp.util.exception.InconsistencyException;
-import minicp.util.exception.NotImplementedException;
+
 public class tp2 extends AbstractConstraint {
+
     private final IntVar[] y;
     private final IntVar x, z;
 
-     /**
-     * Creates a constraint such
-     * that z ≥ 0
-     * si x = 0 alors moyenne(y[0], ..., y[n − 1]) ≤ 2 ∗ z sinon moyenne(y[0], ..., y[n − 1]) ≤ z
-     * @param x verify if equals to 0
-     * @param y the variable
-     * @param v the right member
-     * @see 
-     */
-    public tp2(IntVar x, IntVar[] y, IntVar z) { 
+    public tp2(IntVar x, IntVar[] y, IntVar z) {
         super(y[0].getSolver());
         this.y = y;
         this.x = x;
         this.z = z;
-       
     }
 
     @Override
@@ -46,8 +34,12 @@ public class tp2 extends AbstractConstraint {
 
         if (x.isFixed() && x.min() == 0) {
             z.removeAbove((int) Math.ceil(meanMax / 2.0));
-        } else if (x.isFixed() && x.min() != 0) {
+        } else if (x.isFixed()) {
             z.removeAbove((int) Math.ceil(meanMax));
         }
+    }
+
+    public static tp2 makeTp2(IntVar x, IntVar[] y, IntVar z) {
+        return new tp2(x, y, z);
     }
 }
